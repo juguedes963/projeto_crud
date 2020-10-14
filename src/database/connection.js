b4baab9db4ea927d9d5ab402978bc438ebd70843
jguedes <juliogeeek@gmail.com>
2020-10-13 21:54:04 -0300

    teste

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,4 +1,7 @@
 const { Sequelize,DataTypes,Model,QueryTypes } = require('sequelize');
+const sqlite3=require("sqlite3").verbose()
+const db=new sqlite3.Database('bancoProjeto.db')
+console.log(db)
 const sequelize = new Sequelize('sqlite::memory:')
 const queryInterface=sequelize.getQueryInterface()
 try {
